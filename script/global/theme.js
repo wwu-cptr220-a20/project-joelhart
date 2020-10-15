1b8eeec59a78784652204f95f7bff7e7b094c4e8
Joel Hartman <65033855+joelhart@users.noreply.github.com>
2020-10-14 22:29:05 -0700

    stage-02-attempt-01

---
+++ script/global/theme.js
@@ -0,0 +1,9 @@
+// document.getElementsByClassName("droplist").style.display = "none";
+
+// function button_on() {
+//     document.getElementsByClassName("droplist").style.visibility = "visible";
+// }
+
+// function button_off() {
+//     document.getElementsByClassName("droplist").style.visibility = "hidden";
+// }
